efda30eb5dc16bb6766418de2f06c3f5a0f6470b
yebinnnnn <prettybird1@naver.com>
2024-11-13 16:14:53 +0900

    add two Repository fuction

--- src/main/java/com/trashhcan/letter/repository/LetterJpaRepository.java
+++ src/main/java/com/trashhcan/letter/repository/LetterJpaRepository.java
@@ -10,5 +10,6 @@ import java.util.List;
 @Repository
 public interface LetterJpaRepository extends JpaRepository<Letter, Long> {
     //LetterBox 로 Letter 찾아 최신순 정렬 반환
-    List<Letter> findByLetterBoxOrderByCreatedAtDesc(LetterBox letterBox);
+    List<Letter> findByLetterBoxOrderByCreatedAtDesc(LetterBox letterBox); //letterbox의 편지
+    List<Letter> findByMemberId(Long memberId);//특정 Member id 가 쓴 편지
 }
